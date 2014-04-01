96921e3f70e9827ca25226078befa6a1b1687f11
Jason Trill <jason@jasontrill.com>
2014-04-01 04:48:12 -0700

    Switch to promises for cards.json creation

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 var fs = require('fs');
 var _ = require('lodash');
+var Promise = require('bluebird');
 
-fs.readFile('cards.tsv', {encoding:'utf8'}, function(err, data) {
-  if (err)
-    return console.log(err);
-  var cards = require('tsv').parse(data).slice(0,-1);
-  console.log(cards);
-  fs.readFile('cardPages.json', {encoding:'utf8'}, function(err, data){
-    if (err)
-      return console.log(err);
-    fs.writeFile('cards.json', JSON.stringify(cards), function(err) {
-      if (err)
-        return console.log(err)
+var readFile = Promise.promisify(fs.readFile);
+var writeFile = Promise.promisify(fs.writeFile);
+
+Promise.all([readFile('cards.tsv', 'utf8'), readFile('cardUrls.txt', 'utf8')])
+  .spread(function(cardsTSV,urlsTXT) {
+    var urls = urlsTXT.split('\n').slice(0,-1);
+    var cards = require('tsv').parse(cardsTSV).slice(0,-1).map(function(el, i){
+      el.url = urls[i];
+      return el;
     });
-  
-  }) 
-});
+    return cards;
+  })
+  .then(function(data){
+    console.log(data);
+    return writeFile('cards.json',JSON.stringify(data));
+  })
+  .catch(function(e){
+    console.log('Error', e); 
+  });
+
